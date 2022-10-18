f44cbedb5b6f1cc9853b75cf7cf135c285993c7a
usoobin <112929005+usoobin@users.noreply.github.com>
2022-10-18 16:20:58 +0900

    01

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	int flag =1;
+	int y; 
+	
+	while(flag != 0)
+	{
+		int y; //int y의 범위가 while에만 한정되어있어
+		y = 3;
+		flag = 0;
+	}
+	
+	y=4;
+
+
+	return 0;
+}
