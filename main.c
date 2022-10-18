35edcd6ac0f98f0217335d0991de2842205e3832
usoobin <112929005+usoobin@users.noreply.github.com>
2022-10-18 17:14:35 +0900

    06_2

--- main.c
+++ main.c
@@ -3,12 +3,11 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int all_files;
+static int all_files; //전역범수에 static을 붙이면 범위제한
 
 extern void sub();
 
-
-int main(int argc, char *argv[]) 
+int main(void) 
 {
 	sub();
 	printf("%d\n", all_files);
